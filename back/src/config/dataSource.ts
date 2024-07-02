33b0a4bcbd04a75959b58af6859a760596d50fa3
Guastalli Alexis Dante <165845724+ADGuastalli@users.noreply.github.com>
2024-07-02 18:11:28 -0300

    hola

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -14,7 +14,7 @@ export const AppDataSource = new DataSource({
   password: DB_PASSWORD,
   database: DB_NAME,
   synchronize: true,
-  // dropSchema: true,
+  dropSchema: true,
   logging: false,
   entities: [User, Credential, Order, Product, Category],
   subscribers: [],
