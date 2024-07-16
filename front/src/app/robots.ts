d9b307b07274be75fbb3a027e046da6265504357
Guastalli Alexis Dante <165845724+ADGuastalli@users.noreply.github.com>
2024-07-16 19:27:15 -0300

    fiabnl12

--- front/src/app/robots.ts
+++ front/src/app/robots.ts
@@ -1,16 +1,14 @@
 import Sitemap from "./sitemap";
 
 export default function Robots() {
-  const sitemapUrls = Sitemap();
-  let robotsContent = "";
-
-  robotsContent += "User-agent: *\n";
-  robotsContent += "Allow: /home\n";
-  robotsContent += "Disallow: /dashboard\n\n";
-
-  sitemapUrls.forEach((url) => {
-    robotsContent += `Sitemap: ${url}\n`;
-  });
-
-  return robotsContent;
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/home",
+        disallow: "/dashboard",
+      },
+    ],
+    sitemap: Sitemap(),
+  };
 }
