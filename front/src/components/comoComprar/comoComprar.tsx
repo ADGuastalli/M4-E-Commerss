258ae46df1539da75fbd5662f2a7dbaa2c8603c0
Guastalli Alexis Dante <165845724+ADGuastalli@users.noreply.github.com>
2024-07-16 19:31:09 -0300

    fianl13

--- front/src/components/comoComprar/comoComprar.tsx
+++ front/src/components/comoComprar/comoComprar.tsx
@@ -12,11 +12,11 @@ const Accordion: React.FC<IAccordionProps> = ({ items }) => {
   return (
     <div id="accordion-collapse" data-accordion="collapse">
       {items.map((item) => (
-        <div key={item.id}>
+        <div key={item.id} className="mb-4">
           <h3 id={`accordion-collapse-heading-${item.id}`}>
             <button
               type="button"
-              className="flex items-center justify-between w-full p-5 text-xl font-medium rtl:text-right text-white border border-b-1 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200  hover:bg-gray-300 hover:text-black hover:shadow-md hover:shadow-white focus:bg-gray-300/70 focus:text-black"
+              className="flex items-center justify-between w-full p-5 text-xl font-medium rtl:text-right text-white border border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 hover:bg-gray-300 hover:text-black hover:shadow-md hover:shadow-white focus:bg-gray-300/70 focus:text-black transition-all duration-300"
               data-accordion-target={`#accordion-collapse-body-${item.id}`}
               aria-expanded={expandedItem === item.id}
               aria-controls={`accordion-collapse-body-${item.id}`}
@@ -25,7 +25,7 @@ const Accordion: React.FC<IAccordionProps> = ({ items }) => {
               <span>{item.title}</span>
               <svg
                 data-accordion-icon
-                className={`w-3 h-3 transform ${
+                className={`w-3 h-3 transform transition-transform duration-300 ${
                   expandedItem === item.id ? "rotate-180" : ""
                 }`}
                 aria-hidden="true"
@@ -45,10 +45,14 @@ const Accordion: React.FC<IAccordionProps> = ({ items }) => {
           </h3>
           <div
             id={`accordion-collapse-body-${item.id}`}
-            className={`${expandedItem === item.id ? "" : "hidden"}`}
+            className={`transition-all duration-300 ${
+              expandedItem === item.id
+                ? "max-h-screen"
+                : "max-h-0 overflow-hidden"
+            }`}
             aria-labelledby={`accordion-collapse-heading-${item.id}`}
           >
-            <div className="p-5 border border-b-1 border-gray-200 bg-slate-200/20">
+            <div className="p-5 border border-gray-200 bg-slate-200/20">
               <p className="mb-2 text-white">{item.content}</p>
             </div>
           </div>
