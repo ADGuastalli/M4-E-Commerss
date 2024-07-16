d28950e18f49dffd2be546b3850f47e85affeeb6
Guastalli Alexis Dante <165845724+ADGuastalli@users.noreply.github.com>
2024-07-16 19:25:28 -0300

    fianl12

--- front/src/app/generateRobotsTxt.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import Robots from "./robots";
-
-function generateRobotsTxt() {
-  const robotsContent = Robots();
-  fs.writeFileSync("./robots.ts", robotsContent);
-}
-
-generateRobotsTxt();
