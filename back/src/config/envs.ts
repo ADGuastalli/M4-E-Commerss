d1a62ec60b2a72ef66301ae242574b41bddcaa4a
Guastalli Alexis Dante <165845724+ADGuastalli@users.noreply.github.com>
2024-08-22 13:39:57 -0300

    qqq

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -2,11 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT: number = Number(process.env.PORT) || 3000;
-export const POSTGRES_DATABASE: string =
-  process.env.POSTGRES_DATABASE || "proyecto_m4_front";
-export const POSTGRES_USER: string = process.env.POSTGRES_USER || "postgres";
-export const POSTGRES_PASSWORD: string =
-  process.env.POSTGRES_PASSWORD || "admin";
-export const POSTGRES_HOST: string = process.env.POSTGRES_HOST || "localhost";
+export const POSTGRES_DATABASE = process.env.POSTGRES_DATABASE;
+export const POSTGRES_USER = process.env.POSTGRES_USER;
+export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+export const POSTGRES_HOST = process.env.POSTGRES_HOST;
 export const DB_PORT: number = Number(process.env.DB_PORT) || 5432;
-export const JWT_SECRET: string = process.env.JWT_SECRET || "secret";
+export const JWT_SECRET: string = process.env.JWT_SECRET || "alecen14";
